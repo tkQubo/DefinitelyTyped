b0c6168b0b2a0b811c330dd64889b89d3387a23b
tkQubo <tk.qubo@gmail.com>
2015-08-21 02:30:20 +0900

    Modifying gulp-help (gulp and gulp-help is type-incompatible so far)

--- gulp-help/gulp-help-tests.ts
+++ gulp-help/gulp-help-tests.ts
@@ -1,9 +1,10 @@
+/// <reference path="../gulp/gulp.d.ts" />
 /// <reference path="gulp-help.d.ts" />
 
 'use strict';
 
 import gulpHelp from 'gulp-help';
-var g = gulpHelp(null);
-var gulp = require('gulp-help')(null);
+import { GulpHelp, MethodSignatures } from 'gulp-help';
 
-
+var gulp: gulp.Gulp = require('gulp');
+gulp = gulpHelp(gulp);
