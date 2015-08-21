04273b763012178b75ebeeff458a9a5f38f64187
tkQubo <tk.qubo@gmail.com>
2015-08-21 16:34:38 +0900

    Add gulp-help

--- gulp-help/gulp-help-tests.ts
+++ gulp-help/gulp-help-tests.ts
@@ -3,8 +3,44 @@
 
 'use strict';
 
-import gulpHelp from 'gulp-help';
-import { GulpHelp, MethodSignatures } from 'gulp-help';
+import gulpHelp = require('gulp-help');
 
-var gulp: gulp.Gulp = require('gulp');
-gulp = gulpHelp(gulp);
+var gulp = gulpHelp(require('gulp'));
+
+gulp.task('lint', 'Lints all server side js', function () {
+    var jshint: any;
+
+    gulp.src('./lib/**/*.js')
+        .pipe(jshint());
+});
+
+gulp.task('task-hidden-from-help', false, function () {
+    // ...
+});
+
+gulp.task('version', 'prints the version.', [], function() {
+    // ...
+}, {
+    aliases: ['v', 'V']
+});
+
+gulp.task('version', 'prints the version.', [], function () {
+    // ...
+}, {
+    options: {
+        'env=prod': 'description of env, perhaps with available values',
+        'key=val': 'description of key & val',
+        'key': 'description of key'
+    }
+});
+
+var gulp2 = require('gulp');
+gulpHelp(gulp2, {
+    description: 'Desc',
+    aliases: ['h'],
+    hideEmpty: true,
+    hideDepsMessage: false,
+    afterPrintCallback(): any {
+        console.log('done');
+    }
+});
--- gulp-help/gulp-help.d.ts
+++ gulp-help/gulp-help.d.ts
@@ -6,184 +6,131 @@
 /// <reference path="../bluebird/bluebird.d.ts" />
 /// <reference path="../node/node.d.ts" />
 /// <reference path="../gulp/gulp.d.ts" />
+/// <reference path="../orchestrator/orchestrator.d.ts" />
 
-import Gulp = gulp.Gulp;
-import ITaskCallback = gulp.ITaskCallback;
-import ISrcOptions = gulp.ISrcOptions;
-import EventEmitter = NodeJS.EventEmitter;
-import IWatchCallback = gulp.IWatchCallback;
-import IDestOptions = gulp.IDestOptions;
-import IWatchOptions = gulp.IWatchOptions;
 
-declare type HelpOption = string|boolean;
-declare type WatchCallback = IWatchCallback|string;
-declare type WatchCallbacks = WatchCallback|WatchCallback[];
+declare module "gulp-help" {
+    import Orchestrator = require('orchestrator');
+    import gulp = require('gulp');
+    type HelpOption = string|boolean;
 
-export namespace MethodSignatures {
-    export interface Task {
-        /**
-         * Define a task.
-         *
-         * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
-         * @param help Custom help message as a string. If you want to hide the task from the help menu, supply false
-         * @param deps an array of tasks to be executed and completed before your task will run.
-         * @param fn the function that performs the task's operations. Generally this takes the form of gulp.src().pipe(someplugin()).
-         * @param option task options
-         */
-        (name: string, help: HelpOption, deps: string[], fn?: ITaskCallback, option?: TaskOptions): any;
-        /**
-         * Define a task.
-         *
-         * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
-         * @param help Custom help message as a string. If you want to hide the task from the help menu, supply false
-         * @param deps an array of tasks to be executed and completed before your task will run.
-         * @param option task options
-         */
-        (name: string, help: HelpOption, deps: string[], option?: TaskOptions): any;
-        /**
-         * Define a task.
-         *
-         * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
-         * @param help Custom help message as a string. If you want to hide the task from the help menu, supply false
-         * @param fn the function that performs the task's operations. Generally this takes the form of gulp.src().pipe(someplugin()).
-         * @param option task options
-         */
-        (name: string, help: HelpOption, fn?: ITaskCallback, option?: TaskOptions): any;
-        /**
-         * Define a task.
-         *
-         * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
-         * @param help Custom help message as a string. If you want to hide the task from the help menu, supply false
-         * @param option task options
-         */
-        (name: string, help: HelpOption, option?: TaskOptions): any;
-        /**
-         * Define a task.
-         *
-         * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
-         * @param deps an array of tasks to be executed and completed before your task will run.
-         * @param fn the function that performs the task's operations. Generally this takes the form of gulp.src().pipe(someplugin()).
-         * @param option task options
-         */
-        (name: string, deps: string[], fn?: ITaskCallback, option?: TaskOptions): any;
-        /**
-         * Define a task.
-         *
-         * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
-         * @param deps an array of tasks to be executed and completed before your task will run.
-         * @param option task options
-         */
-        (name: string, deps: string[], option?: TaskOptions): any;
-        /**
-         * Define a task.
-         *
-         * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
-         * @param fn the function that performs the task's operations. Generally this takes the form of gulp.src().pipe(someplugin()).
-         * @param option task options
-         */
-        (name: string, fn?: ITaskCallback, option?: TaskOptions): any;
-        /**
-         * Define a task.
-         *
-         * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
-         * @param option task options
-         */
-        (name: string, option?: TaskOptions): any;
+    namespace gulpHelp {
+        interface TaskMethod {
+            /**
+             * Define a task.
+             *
+             * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
+             * @param help Custom help message as a string. If you want to hide the task from the help menu, supply false
+             * @param deps an array of tasks to be executed and completed before your task will run.
+             * @param fn the function that performs the task's operations. Generally this takes the form of gulp.src().pipe(someplugin()).
+             * @param option task options
+             */
+            (name: string, help: HelpOption, deps: string[], fn?: gulp.TaskCallback, option?: TaskOptions): any;
+            /**
+             * Define a task.
+             *
+             * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
+             * @param help Custom help message as a string. If you want to hide the task from the help menu, supply false
+             * @param deps an array of tasks to be executed and completed before your task will run.
+             * @param option task options
+             */
+            (name: string, help: HelpOption, deps: string[], option?: TaskOptions): any;
+            /**
+             * Define a task.
+             *
+             * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
+             * @param help Custom help message as a string. If you want to hide the task from the help menu, supply false
+             * @param fn the function that performs the task's operations. Generally this takes the form of gulp.src().pipe(someplugin()).
+             * @param option task options
+             */
+            (name: string, help: HelpOption, fn?: gulp.TaskCallback, option?: TaskOptions): any;
+            /**
+             * Define a task.
+             *
+             * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
+             * @param help Custom help message as a string. If you want to hide the task from the help menu, supply false
+             * @param option task options
+             */
+            (name: string, help: HelpOption, option?: TaskOptions): any;
+            /**
+             * Define a task.
+             *
+             * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
+             * @param deps an array of tasks to be executed and completed before your task will run.
+             * @param fn the function that performs the task's operations. Generally this takes the form of gulp.src().pipe(someplugin()).
+             * @param option task options
+             */
+            (name: string, deps: string[], fn?: gulp.TaskCallback, option?: TaskOptions): any;
+            /**
+             * Define a task.
+             *
+             * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
+             * @param deps an array of tasks to be executed and completed before your task will run.
+             * @param option task options
+             */
+            (name: string, deps: string[], option?: TaskOptions): any;
+            /**
+             * Define a task.
+             *
+             * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
+             * @param fn the function that performs the task's operations. Generally this takes the form of gulp.src().pipe(someplugin()).
+             * @param option task options
+             */
+            (name: string, fn?: gulp.TaskCallback, option?: TaskOptions): any;
+            /**
+             * Define a task.
+             *
+             * @param name the name of the task. Tasks that you want to run from the command line should not have spaces in them.
+             * @param option task options
+             */
+            (name: string, option?: TaskOptions): any;
+        }
+
+        interface GulpHelp extends Orchestrator {
+            task: TaskMethod;
+            src: gulp.SrcMethod;
+            dest: gulp.DestMethod;
+            watch: gulp.WatchMethod;
+        }
+
+        interface TaskOptions {
+            /**
+             * List of aliases for this task
+             */
+            aliases?: string[];
+            /**
+             * Object documenting options which can be passed to your task
+             */
+            options?: { [key: string]: string };
+        }
+
+        interface GulpHelpOptions {
+            /**
+             * Modifies the default help message
+             */
+            description?: string;
+            /**
+             * Adds aliases to the default help task
+             */
+            aliases?: string[];
+            /**
+             * Hide all tasks with no help message defined. Useful when including 3rd party tasks
+             */
+            hideEmpty?: boolean;
+            /**
+             * Hide all task dependencies
+             */
+            hideDepsMessage?: boolean;
+            /**
+             * A function to run after the default help task runs
+             */
+            afterPrintCallback?: Function;
+        }
+
     }
 
-    export interface Src {
-        /**
-         * Takes a glob and represents a file structure. Can be piped to plugins.
-         * @param glob a glob string, using node-glob syntax
-         * @param opt an optional option object
-         */
-        (glob: string, opt?: ISrcOptions): NodeJS.ReadWriteStream;
+    function gulpHelp(gulp: gulp.Gulp, options?: gulpHelp.GulpHelpOptions): gulpHelp.GulpHelp;
 
-        /**
-         * Takes a glob and represents a file structure. Can be piped to plugins.
-         * @param glob an array of glob strings, using node-glob syntax
-         * @param opt an optional option object
-         */
-        (glob: string[], opt?: ISrcOptions): NodeJS.ReadWriteStream;
-    }
-
-    export interface Dest {
-        /**
-         * Can be piped to and it will write files. Re-emits all data passed to it so you can pipe to multiple folders.
-         * Folders that don't exist will be created.
-         *
-         * @param outFolder the path (output folder) to write files to.
-         * @param opt
-         */
-        (outFolder: string, opt?: IDestOptions): NodeJS.ReadWriteStream;
-        /**
-         * Can be piped to and it will write files. Re-emits all data passed to it so you can pipe to multiple folders.
-         * Folders that don't exist will be created.
-         *
-         * @param outFolder a function that converts a vinyl File instance into an output path
-         * @param opt
-         */
-        (outFolder: (file: string) => string, opt?: IDestOptions): NodeJS.ReadWriteStream;
-    }
-
-    export interface Watch {
-        /**
-         * Watch files and do something when a file changes. This always returns an EventEmitter that emits change events.
-         *
-         * @param glob a single glob or array of globs that indicate which files to watch for changes.
-         * @param fn a callback or array of callbacks to be called on each change, or names of task(s) to run when a file changes, added with gulp.task().
-         */
-        watch(glob: string|string[], fn: WatchCallbacks): EventEmitter;
-        /**
-         * Watch files and do something when a file changes. This always returns an EventEmitter that emits change events.
-         *
-         * @param glob a single glob or array of globs that indicate which files to watch for changes.
-         * @param opt options, that are passed to the gaze library.
-         * @param fn a callback or array of callbacks to be called on each change, or names of task(s) to run when a file changes, added with gulp.task().
-         */
-        watch(glob: string|string[], opt: IWatchOptions, fn: WatchCallbacks): EventEmitter;
-    }
+    export = gulpHelp;
 }
 
-export interface GulpHelp {
-    task: MethodSignatures.Task;
-    src: MethodSignatures.Src;
-    dest: MethodSignatures.Dest;
-    watch: MethodSignatures.Watch;
-}
-
-export interface TaskOptions {
-    /**
-     * List of aliases for this task
-     */
-    aliases?: string[];
-    /**
-     * Object documenting options which can be passed to your task
-     */
-    options?: { [key: string]: string };
-}
-
-export interface GulpHelpOptions {
-    /**
-     * Modifies the default help message
-     */
-    description: string;
-    /**
-     * Adds aliases to the default help task
-     */
-    aliases: string[];
-    /**
-     * Hide all tasks with no help message defined. Useful when including 3rd party tasks
-     */
-    hideEmpty: boolean;
-    /**
-     * Hide all task dependencies
-     */
-    hideDepsMessage: boolean;
-    /**
-     * A function to run after the default help task runs
-     */
-    afterPrintCallback: Function;
-}
-
-export default function(gulp: Gulp, options?: GulpHelpOptions): GulpHelp;
